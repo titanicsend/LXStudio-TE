968e0f0c5b4d58ec1d0f494370c474649300326a
Ben Hencke <simap@users.noreply.github.com>
2022-04-25 16:38:44 -0700

    Pixelblaze2 (#4)
    * Removed PeriodicPattern class
    
    * WIP: PixelBlaze proof of concept
    
    * WIP: PixelBlaze proof of concept
    
    * Trying and failing to get another pattern working
    
    * Basic Pixelblaze pattern glue w/ reload
    
    * fix indents
    
    * default pixels transparent
    
    * use constant for transparent
    
    * rework wrapper to contain the model points and colors, loop in JS land
    
    this seems to speed it up a bit. Its faster to loop in JS than poke
    variables from Java.
    This makes it a bit easier to partition off a whole wrapper context to
    a subset of some LX points. As a crazy demo of this,
    PixelblazePatternParallel runs a ton of these across a number of
    concurrent threads.
    
    There's definitely some chunkiness happening on first startup, but once
    the JIT/memory warms up it seems smooth.
    
    * Per Mike, need new LX jar to use Click.numLoops, comment out for now
    
    * update Pixelblaze rendering with points, sound, gradients, sliders
    
    point is available in render.
    switch to normalized x,y,z coords for patterns, more compatible with Pixelblaze patterns.
    add sound reactive APIs from Jeff's examples
    add gradient painting APIs, switches based on point type.
    rework PixelblazePattern to be more of a base, extent and override
    script name.
    add slider controls - only works during constructor, so slider has to
    exist when LX loads the pattern initially.
    add a few more color utils
    
    * remove placeholder description
    
    Co-authored-by: Mike Schiraldi <git@schiraldi.org>

---
+++ src/main/java/titanicsend/pattern/pixelblaze/PixelblazePatternParallel.java
@@ -0,0 +1,71 @@
+package titanicsend.pattern.pixelblaze;
+
+import heronarts.lx.LX;
+import heronarts.lx.model.LXPoint;
+import titanicsend.model.TEPanelModel;
+import titanicsend.pattern.TEAudioPattern;
+import titanicsend.pattern.TEPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class PixelblazePatternParallel extends TEAudioPattern {
+  public static final int N_THREADS = 4;
+  private ArrayList<Wrapper> wrappers = new ArrayList<>();
+
+  ExecutorService es = Executors.newFixedThreadPool(N_THREADS);
+
+  public PixelblazePatternParallel(LX lx) {
+    super(lx);
+
+    try {
+
+      //split edge points into chunks
+      int chunksize = (int) Math.ceil((float) model.edgePoints.size() / N_THREADS);
+
+      for (int i = 0; i < model.edgePoints.size(); i += chunksize) {
+        List<LXPoint> chunk = model.edgePoints.subList(i, Math.min(i + chunksize, model.edgePoints.size() - 1));
+        LXPoint[] chunkPoints = new LXPoint[chunk.size()];
+        wrappers.add(Wrapper.fromResource("neon_ice", this, model.edgePoints.toArray(chunkPoints), colors));
+      }
+
+      //one for every panel
+      for (TEPanelModel panelModel : model.panelsById.values()) {
+        wrappers.add(Wrapper.fromResource("xorcery", this, panelModel.points, colors));
+      }
+
+      LX.log("parallel chunks=" + wrappers.size());
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      LX.error(e);
+    }
+  }
+
+  public void run(double deltaMs) {
+    if (wrappers.size() == 0)
+      return;
+    try {
+      updateGradients();
+      computeAudio(deltaMs);
+      ArrayList<Future<Void>> futures = new ArrayList<>(wrappers.size());
+      for (Wrapper wrapper : wrappers) {
+        futures.add(es.submit((Callable<Void>) () -> {
+            wrapper.reloadIfNecessary();
+            wrapper.render(deltaMs);
+            return null;
+        }));
+      }
+
+      for (Future<Void> future : futures) {
+        future.get(); //block until done
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      LX.error(e);
+      return;
+    }
+
+  }
+}
