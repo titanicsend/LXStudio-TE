798e3171bad9926cf5fe431490ade610b1511c4e
Alex Toombs <alex.m.toombs@gmail.com>
2022-05-15 23:05:29 -0700

    script/power_distribution: start calculating panel lines

--- script/power_distribution/calculate_line_lengths.rb
+++ script/power_distribution/calculate_line_lengths.rb
@@ -1,11 +1,15 @@
+require 'matrix'
+
 require './edge.rb'
 require './graph.rb'
 require './panel.rb'
+require './place_junction_boxes.rb'
 require './vertex.rb'
 
 edge_assignments = CSV.read('edge_assignments.tsv', col_sep: "\t")
 vertices = Vertex.load_vertices('../../resources/vehicle/vertexes.txt')
 edges = Edge.load_edges('../../resources/vehicle/edges.txt', vertices)
+panels = Panel.load_panels('../../resources/vehicle/panels.txt', vertices)
 graph = Graph.new(edges: edges, vertices: vertices, panels: panels)
 boxes = place_junction_boxes(graph: graph)
 
@@ -13,9 +17,21 @@ boxes = place_junction_boxes(graph: graph)
 edge_power_length_feet = 0
 panel_power_length_feet = 0
 
+def microns_to_feet(distance_in_microns)
+  distance_in_microns / 304_800
+end
+
 def min_distance_between_vertices_in_feet(graph, vertex1, vertex2)
   distance = graph.min_distance(vertex1, vertex2)
-  distance / 304_800
+  microns_to_feet(distance)
+end
+
+# straight_line_distance calculates the straight line distance between two points.
+# These points may be vertices, but they're not required to be.
+def straight_line_distance(point1, point2)
+  vector1 = Vector[point1[:x], point1[:y], point1[:z]]
+  vector2 = Vector[point2.x, point2.y, point2.z]
+  microns_to_feet((vector1 - vector2).magnitude)
 end
 
 edge_assignments.each do |edge_assignment|
@@ -28,12 +44,25 @@ edge_assignments.each do |edge_assignment|
     next if edge.nil?
 
     edge.vertices.each do |v|
-      edge_power_length_feet += min_distance_between_vertices_in_feet(vertex_id, v.id)
+      edge_power_length_feet += min_distance_between_vertices_in_feet(graph, vertex_id, v.id)
     end
   end
 end
 
-# TODO: calculate power lines to centroids of panels
+# TODO: finish calculating power lines to centroids of panels
+graph.panels.each do |_, panel|
+  centroid = panel.centroid
+
+  # FIXME: why are there panels without closest junction boxes?
+  assigned_junction_box = panel.closest_junction_box
+  if assigned_junction_box.nil?
+    next
+  end
+
+  straight_line_distance_feet = straight_line_distance(centroid, assigned_junction_box.vertex)
+  panel_power_length_feet += panel_power_length_feet
+end
+
 # TODO: calculate ethernet runs along edges to controllers
 
 
