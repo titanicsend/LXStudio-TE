855fa6c50f2186476edba053f37c32c5bdc2163e
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2022-06-28 19:01:04 -0700

    EdgeKITT - Smoother time division changes

--- src/main/java/titanicsend/pattern/jon/EdgeKITT.java
+++ src/main/java/titanicsend/pattern/jon/EdgeKITT.java
@@ -14,13 +14,15 @@ import titanicsend.util.TEMath;
 @LXCategory("Edge FG")
 public class EdgeKITT extends TEAudioPattern {
     float tailPct = 0.5f;
+    float cycleFactor = 2;
+    float lastTimerValue = 0;
 
     public final CompoundParameter energy =
             new CompoundParameter("Energy", .1, 0, 1)
                     .setDescription("Swing!");
 
     protected final CompoundParameter cyclesPerMeasure = (CompoundParameter)
-            new CompoundParameter("Cycles", 2, 1, 4)
+            new CompoundParameter("Cycles", 2, 1, 8)
                     .setExponent(1)
                     .setUnits(LXParameter.Units.INTEGER)
                     .setDescription("Cycles per Measure");
@@ -36,16 +38,16 @@ public class EdgeKITT extends TEAudioPattern {
     public EdgeKITT(LX lx) {
         super(lx);
         addParameter("energy", energy);
-        addParameter("cyclesPerMeasure",cyclesPerMeasure);
-        addParameter("tail",tail);
+        addParameter("cyclesPerMeasure", cyclesPerMeasure);
+        addParameter("tail", tail);
     }
 
-    float square(float n,float dutyCycle) {
+    float square(float n, float dutyCycle) {
         return (Math.abs(TEMath.fract(n)) <= dutyCycle) ? 1.0f : 0.0f;
     }
 
     float trianglef(float n) {
-        return  2f * (0.5f - (float) Math.abs(TEMath.fract(n) - 0.5));
+        return 2f * (0.5f - (float) Math.abs(TEMath.fract(n) - 0.5));
     }
 
     public void runTEAudioPattern(double deltaMs) {
@@ -56,10 +58,18 @@ public class EdgeKITT extends TEAudioPattern {
 
         // 0..1 ramp (sawtooth) of current position within a 4-beat measure.
         float measure = (float) wholeNote();  // lx.engine.tempo.basis()
-        float t1 = (float) TEMath.fract(measure * cyclesPerMeasure.getValuef());
+        float t1 = (float) TEMath.fract(measure * cycleFactor);
+
+        // To keep things as smooth as possible, allow time division multiplier changes only
+        // at the start of a KITT back/forth cycle.  Depending on what else is running, we may
+        // be off by a few ms, but this will get close enough to avoid glitchiness.
+        if (t1 < lastTimerValue) {
+            cycleFactor = (float) Math.floor(cyclesPerMeasure.getValuef());
+        }
+        lastTimerValue = t1;
 
         // use a little exponential magic to implement the energy control swing.
-        t1 = (float) Math.pow(t1,1+energy.getValue());
+        t1 = (float) Math.pow(t1, 1 + energy.getValue());
 
         tailPct = tail.getValuef();
 
@@ -73,9 +83,9 @@ public class EdgeKITT extends TEAudioPattern {
                 float pct1 = x - t1;
                 float pct2 = -x - t1;
 
-                float w1 = Math.max(0f,( tailPct - 1f + trianglef(pct1) * square(pct1,.5f) ) / tailPct );
-                float w2 = Math.max(0f,( tailPct - 1f + trianglef(pct2) * square(pct2,.5f) ) / tailPct );
-                float bri = (w1*w1)+(w2*w2);  // gamma correct both waves before combining
+                float w1 = Math.max(0f, (tailPct - 1f + trianglef(pct1) * square(pct1, .5f)) / tailPct);
+                float w2 = Math.max(0f, (tailPct - 1f + trianglef(pct2) * square(pct2, .5f)) / tailPct);
+                float bri = (w1 * w1) + (w2 * w2);  // gamma correct both waves before combining
                 bri = bri * 255f;  // scale for output
 
                 // clear and reset alpha channel
