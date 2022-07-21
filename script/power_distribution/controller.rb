559028c6160c9c803f2c1ccde6797f682f2aee31
Alex Toombs <alex.m.toombs@gmail.com>
2022-07-21 00:49:36 -0700

    power_distribution: improve debug logging

--- script/power_distribution/controller.rb
+++ script/power_distribution/controller.rb
@@ -182,6 +182,7 @@ def validate_controller_distance_to_first_pixel_edge(controller:, edge:, graph:)
   min_distance_from_controller_to_signal_injection_feet = min_distance_between_vertices_in_feet(graph, controller.vertex.id, edge.signal_in_vertex.id)
 
   if min_distance_from_controller_to_signal_injection_feet > MAX_CONTROLLER_DISTANCE_SIGNAL_TO_FIRST_PIXEL_FEET
+    puts "edge #{edge.id} is #{min_distance_from_controller_to_signal_injection_feet} feet away; vertices: #{edge.vertices.map(&:id)}; controller vertex: #{controller.vertex.id}; edge signal_in_vertex: #{edge.signal_in_vertex.id}"
     raise "error: edge #{edge.id} is too far from controller at #{controller.vertex.id}"
   end
 end
