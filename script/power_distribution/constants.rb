55a6ddc2c01cfa5e770b18c0e8bd920a3cb884f2
Alex Toombs <alex.m.toombs@gmail.com>
2022-08-12 17:07:44 -0700

    signal paths: fix again after Jeff's inventory
    Correct things after this: https://titanicsend.slack.com/archives/C03650K7AE7/p1660342944020329

--- script/power_distribution/constants.rb
+++ script/power_distribution/constants.rb
@@ -26,31 +26,16 @@ MAX_CONTROLLER_DISTANCE_SIGNAL_TO_FIRST_PIXEL_FEET = 7.1
 EDGE_BUILD_PRIORITY_LOW = 'Low'
 
 # See status here: https://docs.google.com/spreadsheets/d/1dQS0hEfm1FXL7tJPlmNtxtZmMggipbxqrDQaaxlxFmo/edit#gid=0
+# And actually, some of these are recategorized to medium/high now.
 BUILT_LOW_PRIORITY_EDGES = [
-    '25-114',
     '25-84',
     '25-88',
     '27-109',
     '27-110',
-    '36-56',
     '52-75',
-    '55-92',
     '58-96',
     '73-81',
     '73-92',
-    '78-97',
-    '79-97',
-    '83-85',
     '84-86',
     '88-110'
-]
-
-BUILT_LOW_PRIORITY_EDGES_STILL_IN_PV = [
-    '9-83',
-    '52-96',
-    '75-96',
-    '91-129',
-    '96-97',
-    '97-128',
-    '114-116',
 ]
