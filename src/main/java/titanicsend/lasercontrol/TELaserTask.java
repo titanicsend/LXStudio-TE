7db235f67e80798cf07e05d85d4ea6d542b022ff
Justin Belcher <jkbelcher@gmail.com>
2023-04-27 13:05:05 -0500

    Adjust LXComponent setup

--- src/main/java/titanicsend/lasercontrol/TELaserTask.java
+++ src/main/java/titanicsend/lasercontrol/TELaserTask.java
@@ -3,22 +3,27 @@ package titanicsend.lasercontrol;
 import heronarts.lx.LX;
 import heronarts.lx.LXComponent;
 import heronarts.lx.LXLoopTask;
-import heronarts.lx.color.LXColor;
 import heronarts.lx.color.LXDynamicColor;
+import heronarts.lx.parameter.BooleanParameter;
+import heronarts.lx.parameter.BooleanParameter.Mode;
 import titanicsend.app.autopilot.TEOscMessage;
 import titanicsend.pattern.TEPattern;
-import titanicsend.util.TE;
 
 public class TELaserTask extends LXComponent implements LXLoopTask {
-    public boolean enabled = true;
-    public LX lx;
+
+    public final BooleanParameter enabled =
+        new BooleanParameter("Enabled", true)
+        .setMode(Mode.TOGGLE);
+
     public TELaserTask(LX lx) {
-        this.lx = lx;
+        super(lx);
+
+        addParameter("enabled", this.enabled);
     }
 
     @Override
     public void loop(double deltaMs) {
-        if (enabled) {
+        if (this.enabled.isOn()) {
             // get the swatch color
             int primaryIndex = TEPattern.ColorType.PRIMARY.swatchIndex();
             LXDynamicColor primary = this.lx.engine.palette.swatch.getColor(primaryIndex);
