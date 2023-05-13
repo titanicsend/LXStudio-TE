c0a68e08582e6c5764370ac5fface8a2a2e57583
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2023-05-13 08:03:27 -0700

    Even More Patterns: RadialSimplex and WavyEdges (#243)
    ### WavyEdges (and WavyPanels)
    Note: This pattern is most often used in WavyEdges form.  The
    update roughly recreates the gradient produced by using WavyEdges
    with the Colorize effect.
    
    - Speed: Yes
    - X/Y Offset: No
    - Spin/Angle: No
    - Brightness: Yes
    - Size: Yes. Scales pattern wavelength, most effective in WavyEdges
    - Quantity: Increase/decrease size and activity of features
    - Wow1: No
    - Wow2: No
    - WowTrigger: No
    
    ### RadialSimplex
    Produces a variety of radial noise textures
    
    - Speed: Yes
    - X/Y Offset: Yes
    - Spin/Angle: Yes...
    - Brightness: Yes
    - Size: Yes
    - Quantity: Adjusts amount of visible noise texture
    - Wow1: Output mode: Radial clouds, concentric circles, fountain of rings
    - Wow2: Foreground color vs. gradient balance
    - WowTrigger: No

---
+++ src/main/java/titanicsend/pattern/jon/RadialSimplex.java
@@ -0,0 +1,85 @@
+package titanicsend.pattern.jon;
+
+import heronarts.lx.LX;
+import heronarts.lx.LXCategory;
+import heronarts.lx.parameter.EnumParameter;
+import titanicsend.pattern.TEPerformancePattern;
+import titanicsend.pattern.yoffa.effect.NativeShaderPatternEffect;
+import titanicsend.pattern.yoffa.framework.PatternTarget;
+import titanicsend.pattern.yoffa.framework.TEShaderView;
+import titanicsend.pattern.yoffa.shader_engine.NativeShader;
+
+@LXCategory("Noise")
+public class RadialSimplex extends TEPerformancePattern {
+
+    NativeShaderPatternEffect effect;
+    NativeShader shader;
+
+    public enum NoiseMode {
+        CLOUDS("Smoke Rings"),
+        CIRCLES("Circles"),
+        FOUNTAIN("Fountain");
+
+        private final String label;
+
+        private NoiseMode(String label) {
+            this.label = label;
+        }
+
+        @Override
+        public String toString() {
+            return this.label;
+        }
+    }
+
+    public final EnumParameter<RadialSimplex.NoiseMode> noiseMode =
+        new EnumParameter<RadialSimplex.NoiseMode>("Mode", RadialSimplex.NoiseMode.CLOUDS)
+            .setDescription("Radial Noise Mode");
+
+
+
+    public RadialSimplex(LX lx) {
+        super(lx);
+
+        // common controls setup
+        controls.setRange(TEControlTag.SPEED, 0, -4, 4);
+        controls.setValue(TEControlTag.SPEED, 0.25);
+
+        controls.setRange(TEControlTag.SIZE, 5, 9, 2);
+        controls.setRange(TEControlTag.QUANTITY, 1,2.5 , .6);
+
+        // noiseMode (in Wow1 control position)
+        controls.setControl(TEControlTag.WOW1,noiseMode);
+
+        // register common controls with LX
+        addCommonControls();
+
+        effect = new NativeShaderPatternEffect("radial_simplex.fs",
+            new PatternTarget(this, TEShaderView.ALL_POINTS));
+    }
+
+    @Override
+    protected void runTEAudioPattern(double deltaMs) {
+
+        RadialSimplex.NoiseMode mode = noiseMode.getEnum();
+        shader.setUniform("iWow1",(float) mode.ordinal());
+
+        // run the shader
+        effect.run(deltaMs);
+    }
+
+    @Override
+    // THIS IS REQUIRED if you're not using ConstructedPattern!
+    // Initialize the NativeShaderPatternEffect and retrieve the native shader object
+    // from it when the pattern becomes active
+    protected void onActive() {
+        super.onActive();
+        effect.onActive();
+        shader = effect.getNativeShader();
+    }
+
+    @Override
+    public String getDefaultView() {
+        return effect.getDefaultView();
+    }
+}
