a9dae79add3e77d14ad89d055f684bb3e9ffb189
theyoffa <104281236+theyoffa@users.noreply.github.com>
2023-05-18 01:27:25 -0700

    fix alpha out of bounds (#278)
    Co-authored-by: yoffa <affoyekim@gmail.com>

--- src/main/java/titanicsend/pattern/yoffa/effect/shaders/FragmentShaderEffect.java
+++ src/main/java/titanicsend/pattern/yoffa/effect/shaders/FragmentShaderEffect.java
@@ -54,12 +54,15 @@ public abstract class FragmentShaderEffect extends PatternEffect {
         double[] colorRgb = getColorForPoint(fragCoordinates, resolution, timeSec);
         //most shaders ignore alpha but optionally plumbing it through is helpful,
         // esp if we want to layer underneath it. can change black background to transparent, etc.
+        for (int i = 0; i < 3; i++) {
+            colorRgb[i] = clamp(colorRgb[i], 0, 1);
+        }
         float alpha = colorRgb.length > 3 ? (float) colorRgb[3] :
-                (float) Math.max(colorRgb[0], Math.max(colorRgb[2], colorRgb[2]));
+                (float) Math.max(colorRgb[0], Math.max(colorRgb[1], colorRgb[2]));
         return new Color(
-                (float) clamp(colorRgb[0], 0, 1),
-                (float) clamp(colorRgb[1], 0, 1),
-                (float) clamp(colorRgb[2], 0, 1),
+                (float) colorRgb[0],
+                (float) colorRgb[1],
+                (float) colorRgb[2],
                 alpha).getRGB();
     }
 
