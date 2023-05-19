2a8b24afba1e2730afbfa614e68a814a0438e1ca
yoffa <affoyekim@gmail.com>
2023-05-19 02:07:23 -0700

    bring back electric dispersion

--- src/main/java/titanicsend/pattern/jon/ElectricEdges.java
+++ src/main/java/titanicsend/pattern/jon/ElectricEdges.java
@@ -20,9 +20,9 @@ public class ElectricEdges extends TEPerformancePattern {
         // pattern, but it is parameterized *very* differently.
         controls.setRange(TEControlTag.SIZE, 0.05, 0.005, 0.4);    // noise scale
         controls.setRange(TEControlTag.SPEED, 1, -2, 2);           // arc wave speed
-        controls.setRange(TEControlTag.QUANTITY, 0.02, 0.0, 0.2);  // noise field amplitude
+        controls.setRange(TEControlTag.QUANTITY, 0.5, 0.5, 1);         // base glow level
         controls.setRange(TEControlTag.WOW1, 0.8, 0, 2.6);         // radial coord distortion
-        controls.setRange(TEControlTag.WOW2, 0.5, 0.5, 1);         // base glow level
+        controls.setRange(TEControlTag.WOW2, 0.02, 0.0, 0.2);  // noise field amplitude
         controls.setRange(TEControlTag.SPIN, 0.6, -3, 3);          // arc spin rate
 
         // register common controls with LX
