b7ee838674037a44108c8b070a1bfab10fa5e422
Titanic's End <96307973+titanicsend@users.noreply.github.com>
2023-05-18 20:37:01 -0700

    Added xerox mode to HandTracker (#289)

--- src/main/java/titanicsend/pattern/cesar/HandTracker.java
+++ src/main/java/titanicsend/pattern/cesar/HandTracker.java
@@ -14,17 +14,32 @@ import static titanicsend.util.TEColor.TRANSPARENT;
 
 @LXCategory("Panel FG")
 public class HandTracker extends TEPattern {
+    public final CompoundParameter junk1 =
+            new CompoundParameter("Junk1", 0, 0, 100)
+                    .setDescription("Placeholder 1");
+    public final CompoundParameter junk2 =
+            new CompoundParameter("Junk2", 0, 0, 100)
+                    .setDescription("Placeholder 2");
+    public final CompoundParameter junk3 =
+            new CompoundParameter("Junk3", 0, 0, 100)
+                    .setDescription("Placeholder 3");
+    public final CompoundParameter targetY =
+            new CompoundParameter("Altitude", 25.5, 0, 100)
+                    .setDescription("Target height from ground");
+    public final CompoundParameter targetZ =
+            new CompoundParameter("Azimuth", 61, -100, 100)
+                    .setDescription("Target position left and right");
+    public final CompoundParameter targetH =
+            new CompoundParameter("Height", 4.2, 1, 100)
+                    .setDescription("Target height");
+
+    public final CompoundParameter targetW =
+            new CompoundParameter("Width", 5, 1, 100)
+                    .setDescription("Target width");
+
     public final LinkedColorParameter color =
             registerColor("Color", "color", ColorType.PRIMARY,
                     "Color of the pattern");
-
-    public final CompoundParameter targetY =
-            new CompoundParameter("Target height", 5, 0, 200)
-                    .setDescription("Target height from ground");
-
-    public final CompoundParameter targetZ =
-            new CompoundParameter("Target position", 5, 0, 100)
-                    .setDescription("Target position left and right");
 
     public final StringParameter indexTip =
             new StringParameter("Index Tip", "10,50")
@@ -32,22 +47,36 @@ public class HandTracker extends TEPattern {
 
     public HandTracker(LX lx) {
         super(lx);
-        addParameter("targetZ", this.targetZ);
-        addParameter("targetY", this.targetY);
+
+        // Three placeholders just to put all four real controls
+        // on the same MIDI knob row
+        addParameter("junk1", this.junk1);
+        addParameter("junk2", this.junk2);
+        addParameter("junk3", this.junk3);
+
+        addParameter("Altitude", this.targetY);
+        addParameter("Azimuth", this.targetZ);
+        addParameter("Height", this.targetH);
+        addParameter("Width", this.targetW);
         addParameter("indexTip", this.indexTip);
     }
 
     @Override
     public void run(double deltaMs) {
         float y = this.targetY.getValuef();
-        float z = this.targetZ.getValuef();
+        float z = -this.targetZ.getValuef();
 
         int color = this.color.calcColor();
 
+        float zMax = this.modelTE.boundaryPoints.maxZBoundaryPoint.z;
+        float yMax = this.modelTE.boundaryPoints.maxYBoundaryPoint.y;
+
         for (LXPoint point : this.modelTE.points) {
             if (this.modelTE.isGapPoint(point)) continue;
-            boolean isCloseToZ = Math.abs(Math.floor(point.zn * 100) - z) < 5;
-            boolean isCloseToY = Math.abs(Math.floor(point.yn * 100 * 2) - y) < 10;
+            float zPercent = 100.0f * point.z / zMax;
+            float yPercent = 100.0f * point.y / yMax;
+            boolean isCloseToZ = Math.abs(Math.floor(zPercent) - z) < targetW.getValue();
+            boolean isCloseToY = Math.abs(Math.floor(yPercent) - y) < targetH.getValue();
             if (isCloseToZ && isCloseToY) {
                 colors[point.index] = color;
             } else {
