21197aad5135dab70c8c2df5563ee8f49309b74f
yoffa <affoyekim@gmail.com>
2023-07-04 16:53:34 -0400

    dont flash

--- src/main/java/titanicsend/effect/NoGapEffect.java
+++ src/main/java/titanicsend/effect/NoGapEffect.java
@@ -10,15 +10,12 @@ import titanicsend.pattern.TEPattern;
 @LXCategory("Titanics End")
 public class NoGapEffect extends TEEffect {
 
-  private static long FLASH_DURATION_MILLIS = 1000;
-  private static long TIMESTAMP = System.currentTimeMillis();
-
   public final BooleanParameter showGapPixels = new BooleanParameter("Flash Gaps")
                   .setDescription("Blinks gap pixels red so they are visible");
 
   public NoGapEffect(LX lx) {
     super(lx);
-    addParameter("Flash Gaps", showGapPixels);
+    addParameter("Show Gaps", showGapPixels);
   }
   
   @Override
@@ -34,12 +31,7 @@ public class NoGapEffect extends TEEffect {
   @Override
   protected void run(double deltaMs, double enabledAmount) {
     // There is only one LXPoint instance for all gap pixels
-    colors[this.modelTE.getGapPointIndex()] = showGapPixels.isOn() && flashOn() ? LXColor.RED
-            : TEPattern.GAP_PIXEL_COLOR;
-  }
-
-  private boolean flashOn() {
-    return (System.currentTimeMillis() - TIMESTAMP) / FLASH_DURATION_MILLIS % 2 == 0;
+    colors[this.modelTE.getGapPointIndex()] = showGapPixels.isOn() ? LXColor.RED : TEPattern.GAP_PIXEL_COLOR;
   }
 
 }
