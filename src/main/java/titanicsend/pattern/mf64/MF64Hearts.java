9c7c23520ee0bab36660b25f16b04b34c8b06655
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2023-07-27 10:33:14 -0700

    Moar bigger heart pattern

--- src/main/java/titanicsend/pattern/mf64/MF64Hearts.java
+++ src/main/java/titanicsend/pattern/mf64/MF64Hearts.java
@@ -1,19 +1,14 @@
 package titanicsend.pattern.mf64;
 
-import titanicsend.model.TEPanelModel;
+import heronarts.lx.model.LXPoint;
 import titanicsend.pattern.TEMidiFighter64DriverPattern;
-import titanicsend.pattern.jon.VariableSpeedTimer;
 import titanicsend.util.TEMath;
 
 import java.util.Random;
 
-import static titanicsend.util.TEColor.TRANSPARENT;
-
 public class MF64Hearts extends TEMidiFighter64Subpattern {
     boolean active;
     boolean stopRequest;
-    VariableSpeedTimer time;
-    float eventStartTime;
     long seed;
     Random prng;
 
@@ -24,8 +19,6 @@ public class MF64Hearts extends TEMidiFighter64Subpattern {
 
         seed = System.currentTimeMillis();
         prng = new Random(seed);
-        time = new VariableSpeedTimer();
-        eventStartTime = -99f;
     }
 
     @Override
@@ -43,19 +36,14 @@ public class MF64Hearts extends TEMidiFighter64Subpattern {
 
     private void startNewEvent() {
         seed = System.currentTimeMillis();
-        eventStartTime = -time.getTimef();
-    }
-
-    private double fract(double n) {
-        return (n - Math.floor(n));
     }
 
     // heart sdf ported from:
     // https://github.com/zranger1/PixelblazePatterns/blob/master/2D_and_3D/heartbeat-SDF-2D.js
     // heart plus a little extra curvature to look nice on LED displays.
     private double heart(double x, double y, double radius) {
-        // tweak aspect ratio a little for triangular panels
-        x = x / radius * 1.25;
+        // tweak aspect ratio a little for our panels
+        x = x / radius * 0.75;
 
         // signed distance from 1/2 heart, mirrored about x axis
         y = y / radius + 0.4 - Math.sqrt(Math.abs(x));
@@ -66,77 +54,41 @@ public class MF64Hearts extends TEMidiFighter64Subpattern {
     }
 
     private void paintAll(int color) {
-        time.tick();
-
-        // calculate time scale at current bpm
-        time.setScale((float) (driver.getTempo().bpm() / 60.0));
-
-        // grab colors of all currently pressed buttons
-        int[] colorSet = buttons.getAllColors();
-
-        // number of lit panels increases slightly with number of buttons pressed.
-        // TEMath.clamp's min and max indicate percentages of coverage.
-        float litProbability = (float) TEMath.clamp(0.4 + 0.3f * ((float) colorSet.length - 1) / 7f,
-            0.4, 0.7);
-
         // clear the decks if we're getting ready to stop
         if (stopRequest) {
             stopRequest = false;
             active = false;
-            colorSet[0] = TRANSPARENT;
         }
 
         prng.setSeed(seed);
-        int colorIndex = 0;
-        int col;
-        for (TEPanelModel panel : modelTE.getAllPanels()) {
 
-            // see if we're going to draw on this panel or leave it dark
-            boolean isLit = (prng.nextFloat() <= litProbability);
-            if (!isLit) continue;
+        // set up animation timing
+        // NOTE: This animation requires an LX engine beat, either from OSC or INT.
+        double t = 0.5 + 0.5 * Math.sin(TEMath.TAU * driver.getTempo().basis());
+        double heartSize = 0.075 + 0.075 * t;
 
+        for (LXPoint point : modelTE.panelPoints) {
 
-            // exclude the 4 flat-on-z front and back panels because x vs.z
-            // gets weird without time-consuming adjustments.
-            if (panel.getId().length() == 2) continue;
+            // adjust x and y to repeat pattern twice,
+            // roughly centered on large car sections and
+            float x = ((2f * point.zn) % 1f) - 0.5f;
+            x += (point.z > 0f) ? -0.1f : 0.1f;
+            float y = point.yn - 0.18f;
 
-            // set up animation timing
-            double t = time.getTime();
-            double elapsedTime = t - eventStartTime;
-            double t0 = fract(elapsedTime);
-            double sinT = Math.sin(t0);
-            double heartSize = 0.35 + 0.075 * sinT;
-            double brightness = 260 + 44 * sinT;
-
-            // pick a color from our set
-            col = colorSet[colorIndex];
-            colorIndex = (colorIndex + 1) % colorSet.length;
-            double shortAxis = Math.min(panel.xRange, panel.zRange);
-            double xAspect = panel.xRange / shortAxis;
-            double yAspect = panel.yRange / shortAxis;
-
-            // now draw something
-            for (TEPanelModel.LitPointData p : panel.litPointData) {
-                // generate roughly centered and normalized (relative to panel)
-                // coordinates
-
-                double x = xAspect * (p.point.z - panel.centroid.z) / panel.zRange;
-                double y = yAspect * (p.point.y - panel.centroid.y) / panel.yRange;
-
-                // heart sdf returns inverse distance from repeating figure.
-                // 1.0 at center, decreasing as you move out. We use this
-                // to calculate brightness.
-                double d = Math.abs(TEMath.fract(heart(x, y, heartSize) / heartSize));
-                // invert and bump brightness curve just a bit
-                int alpha = (int) Math.min(255, brightness * (1.0 - (d * d)));
-                blendColor(p.point.index, (col & 0x00FFFFFF) | (alpha << 24));
-            }
+            // heart sdf returns inverse distance from repeating figure.
+            // 1.0 at center, decreasing as you move out. We use this
+            // to calculate brightness.
+            double d = heart(x, y, heartSize);
+            // NOTE: at this point, d can have values outside the range 0..1.
+            // We deliberately use the overflow to repeat the heart pattern at
+            // various scales.
+            int alpha = (int) (255 * d);
+            blendColor(point.index, (color & 0x00FFFFFF) | (alpha << 24));
         }
     }
 
     @Override
     public void run(double deltaMsec) {
-        time.tick();
         if (this.active) {
             paintAll(buttons.getCurrentColor());
         }
