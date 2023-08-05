cc84b102973645e1ccb0681d25681a8607b86058
Andrew Look <look@aesthetic.com>
2023-08-05 16:34:13 -0400

    TriangleInfinityWaveform: use ConstructedPattern

--- src/main/java/titanicsend/pattern/look/TriangleInfinityWaveform.java
+++ src/main/java/titanicsend/pattern/look/TriangleInfinityWaveform.java
@@ -5,18 +5,28 @@ import heronarts.lx.LXCategory;
 import titanicsend.pattern.TEPerformancePattern;
 import titanicsend.pattern.jon.TEControlTag;
 import titanicsend.pattern.yoffa.effect.NativeShaderPatternEffect;
+import titanicsend.pattern.yoffa.framework.ConstructedPattern;
+import titanicsend.pattern.yoffa.framework.PatternEffect;
 import titanicsend.pattern.yoffa.framework.PatternTarget;
 import titanicsend.pattern.yoffa.framework.TEShaderView;
 import titanicsend.pattern.yoffa.shader_engine.NativeShader;
 
-@LXCategory("Look Shader Patterns")
-public class TriangleInfinityWaveform extends TEPerformancePattern {
-    NativeShaderPatternEffect effect;
-    NativeShader shader;
+import java.util.List;
 
+@LXCategory("Look Shader Patterns")
+public class TriangleInfinityWaveform extends ConstructedPattern {
     public TriangleInfinityWaveform(LX lx) {
         super(lx, TEShaderView.ALL_POINTS);
+    }
 
+    @Override
+    public void runTEAudioPattern(double deltaMs) {
+        // set uniforms if necessary
+        super.runTEAudioPattern(deltaMs);
+    }
+
+    @Override
+    protected List<PatternEffect> createEffects() {
         controls.setRange(TEControlTag.SPEED, 0.25, 0.05, 2.0);
         // number of "layers" of triangles to apply
         controls.setRange(TEControlTag.QUANTITY, 3.0, 1.0, 9.0);
@@ -27,27 +37,7 @@ public class TriangleInfinityWaveform extends TEPerformancePattern {
         // Wave Multiplier
         controls.setRange(TEControlTag.WOW2, 0.1, 0.0, 1.0);
 
-        // register common controls with LX
-        addCommonControls();
-
-        effect = new NativeShaderPatternEffect("triangle_infinity_waveform.fs",
-                new PatternTarget(this));
+        return List.of(new NativeShaderPatternEffect("triangle_infinity_waveform.fs",
+                new PatternTarget(this)));
     }
-
-    @Override
-    public void runTEAudioPattern(double deltaMs) {
-        // run the shader
-        effect.run(deltaMs);
-    }
-
-    @Override
-    // THIS IS REQUIRED if you're not using ConstructedPattern!
-    // Initialize the NativeShaderPatternEffect and retrieve the native shader object
-    // from it when the pattern becomes active
-    public void onActive() {
-        super.onActive();
-        effect.onActive();
-        shader = effect.getNativeShader();
-    }
-
 }
