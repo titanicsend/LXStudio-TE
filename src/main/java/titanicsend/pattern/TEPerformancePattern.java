4a352c521c8b916ed9c6409cea82b67024482b14
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2023-08-17 15:39:35 -0700

    Disable Explode on Non-shader Patterns (#383)
    * Disable Explode control on Non-shader Patterns
    
    For now, "Explode" effect is only available on GL shader patterns.   This PR disables the control on all Java patterns.
    
    * Clean up logic
    
    let's not be lazy now...

--- src/main/java/titanicsend/pattern/TEPerformancePattern.java
+++ src/main/java/titanicsend/pattern/TEPerformancePattern.java
@@ -662,9 +662,20 @@ public abstract class TEPerformancePattern extends TEAudioPattern {
             // TEControlTag enum
             for (TEControlTag tag : TEControlTag.values()) {
                 LXListenableNormalizedParameter param = controlList.get(tag).control;
-                if (missingControls != null && missingControls.missing_control_tags.contains(tag)) {
+                if (missingControls != null) {
+                    if (missingControls.missing_control_tags.contains(tag)){
+                        markUnused(param);
+                    }
+                }
+
+                // disable explode control if the pattern isn't a shader
+                // (takes advantage of the fact that all shaders have a missingControls file entry,
+                // even if they're not missing any controls.)
+                // TODO - reminder to implement "explode" as an effect so all patterns can use it.
+                else if (param.getLabel().equals(TEControlTag.EXPLODE.getLabel())) {
                     markUnused(param);
                 }
+
                 addParameter(tag.getPath(), param);
             }
 
