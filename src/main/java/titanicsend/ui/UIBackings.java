5dbe64eb84466ccd2b6b4a2bf4a107e557def704
Justin Belcher <jkbelcher@gmail.com>
2023-10-01 08:45:25 -0400

    Remove accidental paragraph breaks

--- src/main/java/titanicsend/ui/UIBackings.java
+++ src/main/java/titanicsend/ui/UIBackings.java
@@ -1,13 +1,13 @@
 /**
  * Copyright 2023- Mark C. Slee, Heron Arts LLC
- * <p>
+ *
  * This file is part of the LX Studio software library. By using
  * LX, you agree to the terms of the LX Studio Software License
  * and Distribution Agreement, available at: http://lx.studio/license
- * <p>
+ *
  * Please note that the LX license is not open-source. The license
  * allows for free, non-commercial use.
- * <p>
+ *
  * HERON ARTS MAKES NO WARRANTY, EXPRESS, IMPLIED, STATUTORY, OR
  * OTHERWISE, AND SPECIFICALLY DISCLAIMS ANY WARRANTY OF
  * MERCHANTABILITY, NON-INFRINGEMENT, OR FITNESS FOR A PARTICULAR
