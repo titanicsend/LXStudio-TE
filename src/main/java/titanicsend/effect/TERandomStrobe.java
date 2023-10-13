e3df6b61d8d259635cb77f0f932f0decdd30321c
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2023-10-13 12:35:25 -0700

    All working.  Fine tuning next

--- src/main/java/titanicsend/effect/TERandomStrobe.java
+++ src/main/java/titanicsend/effect/TERandomStrobe.java
@@ -1,134 +1,139 @@
 package titanicsend.effect;
 
-/**
+/*
  * Minimalist strobe effect for Titanic's End, designed to be run via DMX from
  * a remote lighting console.  Strobes one or more random panels at the set
  * trigger rate.
  *
-
  */
 
 import heronarts.lx.LX;
 import heronarts.lx.LXCategory;
-import heronarts.lx.Tempo;
 import heronarts.lx.color.LXColor;
-import heronarts.lx.model.LXPoint;
-import heronarts.lx.modulator.LXWaveshape;
-import heronarts.lx.modulator.SawLFO;
 import heronarts.lx.parameter.BooleanParameter;
-import heronarts.lx.parameter.BoundedParameter;
 import heronarts.lx.parameter.CompoundParameter;
-import heronarts.lx.parameter.EnumParameter;
-import heronarts.lx.parameter.FunctionalParameter;
-import heronarts.lx.parameter.LXParameter;
-import heronarts.lx.parameter.ObjectParameter;
-import heronarts.lx.utils.LXUtils;
+import titanicsend.model.TEEdgeModel;
+import titanicsend.model.TEPanelModel;
+import titanicsend.pattern.jon.VariableSpeedTimer;
+import titanicsend.util.TEMath;
+
+import java.util.*;
 
 @LXCategory("Titanics End")
 public class TERandomStrobe extends TEEffect {
 
-    public final ObjectParameter<LXWaveshape> waveshape =
-        new ObjectParameter<LXWaveshape>("Waveshape", new LXWaveshape[] {
-            LXWaveshape.SIN,
-            LXWaveshape.TRI,
-            LXWaveshape.SQUARE,
-            LXWaveshape.UP,
-            LXWaveshape.DOWN
-        });
+    private class PanelsWithEdges {
+        public TEPanelModel panel;
+        public Set<TEEdgeModel> edges;
+    }
 
-    public final BoundedParameter maxFrequency =
-        new BoundedParameter("Max Freq", 5, 1, 30)
-            .setDescription("Maximum strobing frequency")
-            .setUnits(LXParameter.Units.HERTZ);
+    private final ArrayList<PanelsWithEdges> panelsWithEdges = new ArrayList<>();
+    private final VariableSpeedTimer time;
+    private double eventStartTime;
 
-    public final BoundedParameter minFrequency =
-        new BoundedParameter("Min Freq", .5, .1, 1)
-            .setDescription("Minimium strobing frequency")
-            .setUnits(LXParameter.Units.HERTZ);
+    private static final int PRIMARY_COLOR_INDEX = 2;
 
     public final CompoundParameter speed =
-        new CompoundParameter("Speed", .5)
-            .setUnits(CompoundParameter.Units.PERCENT_NORMALIZED)
+        new CompoundParameter("Speed", 0, 0, 24)
             .setExponent(2)
-            .setDescription("Speed of the strobe effect");
+            .setDescription("Frequency of strobe effect in Hz");
 
-    public final CompoundParameter depth =
-        new CompoundParameter("Depth", 0.5)
-            .setUnits(CompoundParameter.Units.PERCENT_NORMALIZED)
-            .setDescription("Depth of the strobe effect");
+    public final CompoundParameter density =
+        new CompoundParameter("Density", 8, 1, 26)
+            .setDescription("Number of triangles to strobe");
 
-    public final CompoundParameter bias =
-        new CompoundParameter("Bias", 0, -1, 1)
-            .setUnits(CompoundParameter.Units.PERCENT_NORMALIZED)
-            .setPolarity(CompoundParameter.Polarity.BIPOLAR)
-            .setDescription("Bias of the strobe effect");
+    public final CompoundParameter dutyCycle =
+        new CompoundParameter("DutyCycle", 0.4)
+            .setDescription("Duty cycle of strobe effect");
 
-    public final BooleanParameter tempoSync =
-        new BooleanParameter("Sync", false)
-            .setDescription("Whether to sync the tempo to a clock division");
+    public final BooleanParameter panelsLit =
+        new BooleanParameter("Panels", true)
+            .setDescription("Strobe effect lights panels");
 
-    public final EnumParameter<Tempo.Division> tempoDivision =
-        new EnumParameter<Tempo.Division>("Division", Tempo.Division.QUARTER)
-            .setDescription("Which tempo division to use when in sync mode");
+    public final BooleanParameter edgesLit =
+        new BooleanParameter("Edges", true)
+            .setDescription("Strobe effect lights panel edges");
 
-    public final BoundedParameter tempoPhaseOffset =
-        new BoundedParameter("Phase Offset", 0)
-            .setUnits(CompoundParameter.Units.PERCENT_NORMALIZED)
-            .setDescription("Shifts the phase of the strobe LFO relative to tempo");
-
-    private final SawLFO basis = startModulator(new SawLFO(0, 1, new FunctionalParameter() {
-        @Override
-        public double getValue() {
-            return 1000 / LXUtils.lerp(minFrequency.getValue(), maxFrequency.getValue(), speed.getValue());
-        }}));
+    public final BooleanParameter white =
+        new BooleanParameter("White", false)
+            .setDescription("Strobe is bright white if enabled.");
 
     public TERandomStrobe(LX lx) {
         super(lx);
         addParameter("speed", this.speed);
-        addParameter("depth", this.depth);
-        addParameter("bias", this.bias);
-        addParameter("waveshape", this.waveshape);
-        addParameter("tempoSync", this.tempoSync);
-        addParameter("tempoDivision", this.tempoDivision);
-        addParameter("tempoPhaseOffset", this.tempoPhaseOffset);
-        addParameter("minFrequency", this.minFrequency);
-        addParameter("maxFrequency", this.maxFrequency);
+        addParameter("density", this.density);
+        addParameter("dutyCycle", this.dutyCycle);
+        addParameter("panelsLit", this.panelsLit);
+        addParameter("edgesLit", this.edgesLit);
+        addParameter("white", this.white);
+
+        time = new VariableSpeedTimer();
+        eventStartTime = -99f;
+
+        buildPanelsAndEdgeList();
     }
 
-    @Override
-    protected void onEnable() {
-        this.basis.setBasis(0).start();
-    }
-
-    public float compute(double basis, boolean useBaseValue) {
-        double strobe = this.waveshape.getObject().compute(basis);
-        double bias = useBaseValue ? this.bias.getBaseValue() : this.bias.getValue();
-        double expPower = (bias >= 0) ? (1 + 3*bias) : (1 / (1 - 3*bias));
-        if (expPower != 1) {
-            strobe = Math.pow(strobe, expPower);
+    private PanelsWithEdges findPanelEntry(TEPanelModel p) {
+        for (PanelsWithEdges pwe : panelsWithEdges) {
+            if (pwe.panel == p) return pwe;
         }
-        return LXUtils.lerpf(1, (float) strobe, useBaseValue ? this.depth.getBaseValuef() : this.depth.getValuef());
+        return null;
     }
 
-    private double getTempoBasis() {
-        double basis = this.lx.engine.tempo.getBasis(this.tempoDivision.getEnum());
-        return (basis + this.tempoPhaseOffset.getValue()) % 1.;
+    // Ick! exhaustive search for edges connected to each panel
+    // TODO - Add connectedEdges to panel model. It'll be handy.
+    //
+    // TODO - This is all the panels/edges on the car, which means
+    // TODO - it's technically possible to have all the random strobes on
+    // TODO - one side, leaving the other dark. Can we make use of symmetry
+    // TODO - to avoid this, and speed up frame calculation?
+    public void buildPanelsAndEdgeList() {
+        for (TEEdgeModel edge : modelTE.getAllEdges()) {
+            for (TEPanelModel panel : edge.connectedPanels) {
+                PanelsWithEdges pwe = findPanelEntry(panel);
+                if (pwe == null) {
+                    pwe = new PanelsWithEdges();
+                    pwe.panel = panel;
+                    pwe.edges = new HashSet<>();
+                    panelsWithEdges.add(pwe);
+                }
+                pwe.edges.add(edge);
+            }
+        }
     }
 
     @Override
     public void run(double deltaMs, double enabledAmount) {
-        float amt = (float) enabledAmount * this.depth.getValuef();
-        if (amt > 0) {
-            double strobeBasis = this.tempoSync.isOn() ? getTempoBasis() : this.basis.getValue();
-            float strobe = LXUtils.lerpf(1, compute(strobeBasis, false), (float) enabledAmount);
+        // set time scale for current strobe frequency
+        time.setScale(speed.getValue());
+        time.tick();
 
-            if (strobe < 1) {
-                if (strobe == 0) {
-                    setColors(LXColor.BLACK);
-                } else {
-                    int src = LXColor.gray(100 * strobe);
-                    for (LXPoint p : modelTE.points) {
-                        this.colors[p.index] = LXColor.multiply(this.colors[p.index], src, 0x100);
+        if (speed.getValue() > 0) {
+            double et = time.getTime() - eventStartTime;
+
+            // if we're not currently in a flash event start a new one
+            if (et >= 1.0) {
+                Collections.shuffle(panelsWithEdges);
+                eventStartTime = time.getTime();
+            }
+
+            if (et <= dutyCycle.getValue()) {
+                // TODO - get a real color
+                int col = (white.isOn()) ? LXColor.WHITE : lx.engine.palette.getSwatchColor(PRIMARY_COLOR_INDEX).getColor();;
+
+                for (int i = 0; i < density.getValue(); i++) {
+                    PanelsWithEdges pwe = panelsWithEdges.get(i);
+                    if (panelsLit.isOn()) {
+                        for (TEPanelModel.LitPointData p : pwe.panel.litPointData) {
+                            this.colors[p.point.index] = col;
+                        }
+                    }
+                    if (edgesLit.isOn()) {
+                        for (TEEdgeModel e : pwe.edges) {
+                            for (TEEdgeModel.Point p : e.points) {
+                                this.colors[p.index] = col;
+                            }
+                        }
                     }
                 }
             }
