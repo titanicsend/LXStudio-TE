cf54e9c6946596c5412248cc18ba564bc690ba25
Andrew Look <look@aesthetic.com>
2023-10-17 16:38:11 -0400

    update script/start-lx.sh - update snapshot version, use Oct_21.lxp, comment out logic around lighting laptop / driving pattern

--- script/start-lx.sh
+++ script/start-lx.sh
@@ -1,16 +1,18 @@
 #! /bin/sh
 
-LIGHTING_LAPTOP=10.1.1.10
+#LIGHTING_LAPTOP=10.1.1.10
+#
+#PING_TARGET=${LIGHTING_LAPTOP?}
+#
+#if ping -q -t 5 -c 3 ${PING_TARGET?}; then
+#  echo "$PING_TARGET is reachable; launching AutoVJ"
+#  LXP=Projects/Oct_21.lxp
+#else
+#  echo "$PING_TARGET is not reachable; launching Driving Mode"
+#  LXP=Projects/driving.lxp
+#fi
 
-PING_TARGET=${LIGHTING_LAPTOP?}
-
-if ping -q -t 5 -c 3 ${PING_TARGET?}; then
-  echo "$PING_TARGET is reachable; launching AutoVJ"
-  LXP=Projects/BM_2023.lxp
-else
-  echo "$PING_TARGET is not reachable; launching Driving Mode"
-  LXP=Projects/driving.lxp
-fi
+LXP=Projects/Oct_21.lxp
 
 # then start Chromatik
 # we are assuming that the parent process has already set the correct
@@ -20,5 +22,5 @@ fi
 java \
   -XstartOnFirstThread \
   -Djava.awt.headless=true \
-  -jar target/LXStudio-TE-0.2.2-SNAPSHOT-jar-with-dependencies.jar \
+  -jar target/LXStudio-TE-0.2.3-SNAPSHOT-jar-with-dependencies.jar \
   vehicle ${LXP?};
