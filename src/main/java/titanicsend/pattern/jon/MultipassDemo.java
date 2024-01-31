96a8c9a836e5922cf2762ee59393492ac3ee6d50
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2024-01-31 09:20:56 -0700

    Update MultipassDemo.java

--- src/main/java/titanicsend/pattern/jon/MultipassDemo.java
+++ src/main/java/titanicsend/pattern/jon/MultipassDemo.java
@@ -1,12 +1,8 @@
 package titanicsend.pattern.jon;
 
-import com.jogamp.opengl.util.GLBuffers;
 import heronarts.lx.LX;
 import heronarts.lx.LXCategory;
-import heronarts.lx.parameter.LXParameter;
 import java.nio.ByteBuffer;
-
-import titanicsend.pattern.glengine.GLEngine;
 import titanicsend.pattern.glengine.GLShader;
 import titanicsend.pattern.glengine.GLShaderPattern;
 import titanicsend.pattern.yoffa.framework.TEShaderView;
@@ -26,6 +22,10 @@ public class MultipassDemo extends GLShaderPattern {
     // allocate a backbuffer for all the shaders to share
     buffer = GLShader.allocateBackBuffer();
 
+    // add the first shader, passing in the shared backbuffer
+    shader = new GLShader(lx, "fire.fs", this,buffer);
+    addShader(shader);    
+
     // add the second shader, which applies a simple edge detection filter to the
     // output of the first shader
     shader = new GLShader(lx, "multipass1.fs", this, buffer);
@@ -34,6 +34,4 @@ public class MultipassDemo extends GLShaderPattern {
 
   // additional shaders can be added in the same way. They will be run in the order
   // they are added, and each will be passed the output of the previous shader.
-
-
 }
