e3855869fcba60753ae8faa2f84c6a5a7731c931
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2024-04-06 15:38:08 -0700

    Audio Reactivity for Turbulent Noise Lines (#468)
    * Initial commit
    
    * checkpoint
    
    * TurbulentNoiseLines audio update
    
    Add audio reactivity to Turbulent Noise Lines

--- resources/shaders/turbulent_noise_lines.fs
+++ resources/shaders/turbulent_noise_lines.fs
@@ -4,6 +4,7 @@
 const float PI = 3.1415926;
 const float TAU = 2.0 * PI;
 
+
 // circle function from nimitz @ ShaderToy
 // returns distance from a point to a cirular pulse
 float circle(vec2 p) {
@@ -33,46 +34,59 @@ float noise(vec2 uv) {
 }
 
 // several octaves of value noise make a cloudy-looking turbulent field
+// higher bassLevel increases the amount of "dirt" in the noise field
 float turbulenceNoise(vec2 uv) {
+    float dirt = bassLevel * levelReact * 0.3;
     float k = 4.0;
 
-    //uv = rotate2D(k + 0.00001 * iTime) * uv;
+
     float res = 0.;
-    float c = 0.5;
+    float c = 0.5 + dirt;
 
     for (int i = 0; i < 8; i++) {
-        uv -= iTranslate  * 0.2;
+        uv -= iTranslate * 0.25;
         uv = rotate2D(k + 0.00001 * iTime) * k * uv;
         res += c * noise(uv);
-        c *= 0.5;
+        c *= 0.5 + dirt;
+        dirt *= 0.5;
     }
 
     return res;
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-
     // normalize, scale, rotate
     vec2 uv = (fragCoord.xy - 0.5 * iResolution.xy) / iResolution.x;
+
+    vec2 warp = vec2(60,-60) * bassLevel;
+    float warpAmt = levelReact * 0.02;
+    uv += warpAmt * sin(uv * warp);
+
     uv *= iScale;
     uv = uv * rotate2D(-iRotationAngle);
 
+    // generate base noise field.  This is an 8-octave turbulent fbm, with random-ish rotation
+    // applied to each octave.
     float res = turbulenceNoise(uv);
+    res = res;
     uv = rotate2D(1.65 * noise(uv * 5. + 0.1 * iTime)) * uv;
 
     // Quantity controls the density of the lines derived from the noise field
-    float line = smoothstep(0., 1., abs(res + sin(TAU * res + iQuantity * uv.x)));
-    line = smoothstep(0., 1., line);
+    // Treble "splatters" additional ink on the lines.
+    float splatter = max(0.0,(trebleLevel - 0.15) * 2.5 * frequencyReact);
+    float inkCurve = abs(res + sin(TAU * res + iQuantity * (uv.x+uv.y))) - splatter;
+    float line = smoothstep(0., 1., inkCurve);
 
-    // Wow Trigger runs the TE special dual ring pulse generator, which draws only on
-    // the wavy lines (and not on the background fog.)
-    if (iWowTrigger) {
-        uv /= exp(beat * PI);
-        line *= max(1.0, 0.75 / pow(abs(2.1 - circle(uv)),0.75));
-    }
+    // Add additional light based on treble energey -- this is a dual ring pulse generator,
+    // which draws on its own distorted radial coordinate system to highlight the lines.
+    uv -= sin(uv * warp);
+    uv /= exp(beat * PI);
+    float k = (res / 4.0) * pow(abs(2.1 - circle(uv)),1.25);
+    line += (line > 0.1) ? k * 2.5 * splatter : 0.0;
 
     // Wow1 controls the mix of lines vs. noise field background
-    float bri = mix(1.5 * res, line, iWow1);
+    float bri = mix(res, line, iWow1);
+
     // Wow2 controls the mix of foreground color vs. gradient
     vec3 col = bri * mix(iColorRGB, mix(iColor2RGB, iColorRGB,smoothstep(0.1,0.8, bri * bri)), iWow2);
 
