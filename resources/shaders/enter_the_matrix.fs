41cd7a0fc4c44e3b9c03aa534e150d5e12617097
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2024-04-10 19:06:08 -0700

    make it visible on TE
    and palette compliant too!

--- resources/shaders/enter_the_matrix.fs
+++ resources/shaders/enter_the_matrix.fs
@@ -1,10 +1,11 @@
 #define R fract(1e2 * sin(p.x * 8. + p.y))
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    // shadertoy line:
-    //     vec3 v = vec3(fragCoord / iResolution, 1) - 0.5;
-    // our iResolution is a vec2, not a vec3, so I try to change to the below:
-    vec3 v = vec3(fragCoord.x / iResolution.x - 0.5, fragCoord.y / iResolution.y - 0.5, 1);
+    vec3 v = vec3(fragCoord / iResolution.xy, 1.0) - 0.5;
+
+    // v.z controls the overall scale - need to adjust actual control
+    // values to make this more sane.
+    v.z *= iScale * 0.1;
 
     vec3 s = 0.5 / abs(v);
     s.z = min(s.y, s.x);
@@ -15,8 +16,10 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec3 j = fract(i);
     i -= j;
 
-    vec3 p = vec3(9, int(iTime * (9.0 + 8.0 * sin(i).x)), 0) + i;
-    fragColor.g = R / s.z;
-    p *= j;
-    fragColor *= (R > 0.5 && j.x < 0.6 && j.y < 0.8 ? 1.0 : 0.0);
+    vec3 p = j * vec3(9, int(iTime * (9.0 + 8.0 * sin(i).x)), 0) + i;
+    vec3 color = mix(iColorRGB,iColor2RGB,vec3(R / s.z));
+
+    // the constants compared to R, j.x, and j.y control how filled in each block is
+    // higher values give more fill, lower values, more dither.
+    fragColor = vec4(color * (R > 0.15 && j.x < 0.675 && j.y < 0.675 ? 1.0 : 0.0), 1.0);
 }
