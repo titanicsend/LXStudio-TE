e0dcebfa254983c4737dbf562b995098d70d5181
worldveil <will.drevo@gmail.com>
2024-04-10 17:33:59 -0700

    WIP on matrix pattern

--- resources/shaders/enter_the_matrix.fs
+++ resources/shaders/enter_the_matrix.fs
@@ -1,10 +1,11 @@
-
-#define R fract(1e2*sin(p.x*8.+p.y))
-
+#define R fract(1e2 * sin(p.x * 8. + p.y))
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    //vec3 v = vec3(fragCoord, 1) / iResolution - 0.5;
-    vec3 v = vec3(fragCoord, 1) / vec3(iResolution, 1) - 0.5;
+    // shadertoy line:
+    //     vec3 v = vec3(fragCoord / iResolution, 1) - 0.5;
+    // our iResolution is a vec2, not a vec3, so I try to change to the below:
+    vec3 v = vec3(fragCoord.x / iResolution.x - 0.5, fragCoord.y / iResolution.y - 0.5, 1);
+
     vec3 s = 0.5 / abs(v);
     s.z = min(s.y, s.x);
 
@@ -17,5 +18,5 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec3 p = vec3(9, int(iTime * (9.0 + 8.0 * sin(i).x)), 0) + i;
     fragColor.g = R / s.z;
     p *= j;
-    fragColor *= R > 0.5 && j.x < 0.6 && j.y < 0.8 ? 1.0 : 0.0;
+    fragColor *= (R > 0.5 && j.x < 0.6 && j.y < 0.8 ? 1.0 : 0.0);
 }
