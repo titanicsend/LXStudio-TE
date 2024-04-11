84b3d796f37f29c80aa04d7a10b35b42e1321030
Andrew Look <look@aesthetic.com>
2024-04-10 23:57:45 -0700

    metallicwaves: level controls basstreble effect, freq controls tempo-reactivity

--- resources/shaders/metallic_wave.fs
+++ resources/shaders/metallic_wave.fs
@@ -18,20 +18,23 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec2 uv = fragCoord.xy / iResolution.xy * 2.0 - 1.0;
     uv *= rotate2D(-iRotationAngle);
 
+    float bassTrebleMax = clamp(levelReact * max(bassRatio, trebleRatio / 8.), 0.4, 8);
+    float beatEffect = 1.0 - beat * frequencyReact;
+
     float frequency = iQuantity;
     float speed = iTime / frequency;
     float d = length(uv);
     float a = atan(uv.y, uv.x);
-    uv.x = cos(a) * d * clamp(max(bassRatio, trebleRatio / 8.), 0.4, 8);
-    uv.y = sin(a) * d * clamp(max(bassRatio, trebleRatio / 8.), 0.4, 8);
+    uv.x = cos(a) * d * bassTrebleMax;
+    uv.y = sin(a) * d * bassTrebleMax;
 
     d -= speed;
     // Wow1 controls pixelated decomposition
     uv += sin(uv * 1234.567 + speed) * iWow1 * 0.25;
-    float bpmReactivityAmount = max(1.0 - beat * levelReact , brightness_floor);
+    float bpmReactivityAmount = max(beatEffect, brightness_floor);
     float bri = 0.05 + abs(mod(uv.y + uv.x * frequency * d, uv.x * 2.0)) * bpmReactivityAmount;
 
     // Wow2 controls the mix of foreground color vs. gradient
-    vec3 col = clamp(0.9 + frequencyReact * 0.1, 0, 1) * bri * mix(iColorRGB, mix(iColor2RGB, iColorRGB, wave(bri)), iWow2);
+    vec3 col = bri * mix(iColorRGB, mix(iColor2RGB, iColorRGB, wave(bri)), iWow2);
     fragColor = vec4(col, 1.);
 }
