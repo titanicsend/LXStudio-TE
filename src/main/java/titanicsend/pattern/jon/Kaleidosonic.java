d24b5d3b26f4e4abcf5a33a8b343e1d29e0e2551
worldveil <worldveil@users.noreply.github.com>
2024-04-11 03:17:21 -0400

    Kaleidonsonic updates for reactivity (#476)
    * Kaleidonsonic updates for reactivity
    
    * Move beat reactive size calc
    
    Try this and see if you like it!
    
    I moved the beat reactive size calculation (nice!) out of the blob accumulator loop, since it's per-frame invariant anyway, and set it up so that the pattern won't drop to black if there's no audio signal.
    
    Also removed the references to the new EnterTheMatrix pattern from TEApp.java, since the actual code isn't in this PR.
    
    * I feel slightly better about these parameters
    
    * Revert back
    
    ---------
    
    Co-authored-by: ZRanger1 <35553094+zranger1@users.noreply.github.com>

--- src/main/java/titanicsend/pattern/jon/Kaleidosonic.java
+++ src/main/java/titanicsend/pattern/jon/Kaleidosonic.java
@@ -12,12 +12,12 @@ public class Kaleidosonic extends GLShaderPattern {
   public Kaleidosonic(LX lx) {
     super(lx, TEShaderView.ALL_POINTS);
 
-    controls.setRange(TEControlTag.WOW1, 1., 0, 2.5); // complexity
+    controls.setRange(TEControlTag.WOW1, 0.14, 0.05, 0.25); // complexity
     controls.setRange(TEControlTag.WOW2, 0.5, 0.2, 3); // contrast
     controls.setRange(TEControlTag.SIZE, 2., 3, 0.5); // scale
     controls.setRange(TEControlTag.QUANTITY, 7, 1, 13); // number of kaleidoscope slices
-    controls.setValue(TEControlTag.SPIN, 0.125);
-    controls.setRange(TEControlTag.LEVELREACTIVITY,0.1,0,1);
+    controls.setValue(TEControlTag.SPIN, 0.05);
+    controls.setRange(TEControlTag.LEVELREACTIVITY,0.1,0,0.3);
     controls.setRange(TEControlTag.FREQREACTIVITY,0.1,0,1);
 
     // register common controls with LX
