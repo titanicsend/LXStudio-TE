66cd4617b265230a84b54437b58236c7dbf0f36f
Andrew Look <look@aesthetic.com>
2024-05-26 23:13:32 -0400

    wip: more palette combination options

--- src/main/java/titanicsend/effect/LookColorPaletteEffect.java
+++ src/main/java/titanicsend/effect/LookColorPaletteEffect.java
@@ -24,9 +24,9 @@ import heronarts.lx.LXComponentName;
 import heronarts.lx.color.ColorParameter;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.color.LXDynamicColor;
+import heronarts.lx.color.LXPalette;
 import heronarts.lx.color.LXSwatch;
 import heronarts.lx.effect.LXEffect;
-import heronarts.lx.parameter.BooleanParameter;
 import heronarts.lx.parameter.CompoundParameter;
 import heronarts.lx.parameter.EnumParameter;
 import titanicsend.modulator.dmx.DmxColorModulator;
@@ -72,12 +72,15 @@ public class LookColorPaletteEffect extends LXEffect {
               "Destination color position (1-based) in the global palette current swatch");
 
   public enum PaletteType {
-    TRIAD,
-    ACCENTUATED,
+    MONO,
+    TRIADIC,
+    ANALOGOUS,
+    SPLIT_COMPLEMENTARY,
+    COMPLEMENTARY,
   }
 
   public final EnumParameter<PaletteType> paletteType =
-      new EnumParameter<PaletteType>("Color Position", PaletteType.TRIAD)
+      new EnumParameter<PaletteType>("Color Position", PaletteType.TRIADIC)
           .setDescription(
               "Destination color position (1-based) in the global palette current swatch");
 
@@ -93,6 +96,14 @@ public class LookColorPaletteEffect extends LXEffect {
     addParameter("paletteType", this.paletteType);
   }
 
+  public LXSwatch getActiveSwatch() {
+    return this.lx.engine.palette.swatch;
+  }
+
+  public LXSwatch getCueSwatch() {
+    return findOrCreateCueSwatch();
+  }
+
   @Override
   protected void run(double deltaMs, double amount) {
     float hue = this.hue.getValuef();
@@ -104,29 +115,51 @@ public class LookColorPaletteEffect extends LXEffect {
     int color3 = color;
     this.color.setColor(color);
 
-    if (this.paletteType.getEnum() == PaletteType.TRIAD) {
+    if (this.paletteType.getEnum() == PaletteType.MONO) {
+      color2 = LXColor.BLACK;
+      color3 = LXColor.BLACK;
+    } else if (this.paletteType.getEnum() == PaletteType.COMPLEMENTARY) {
+      color2 = LXColor.hsb(hue + 180, saturation, brightness);
+      color3 = LXColor.BLACK;
+    } else if (this.paletteType.getEnum() == PaletteType.SPLIT_COMPLEMENTARY) {
+      color2 = LXColor.hsb(hue + 150, saturation, brightness);
+      color3 = LXColor.hsb(hue + 210, saturation, brightness);
+    } else if (this.paletteType.getEnum() == PaletteType.TRIADIC) {
       color2 = LXColor.hsb(hue + 120, saturation, brightness);
       color3 = LXColor.hsb(hue + 240, saturation, brightness);
-      this.color2.setColor(color2);
-      this.color3.setColor(color3);
-    } else if (this.paletteType.getEnum() == PaletteType.ACCENTUATED) {
+    } else if (this.paletteType.getEnum() == PaletteType.ANALOGOUS) {
       color2 = LXColor.hsb(hue + 30, saturation, brightness);
       color3 = LXColor.hsb(hue - 30, saturation, brightness);
-      this.color2.setColor(color2);
-      this.color3.setColor(color3);
     }
+    this.color2.setColor(color2);
+    this.color3.setColor(color3);
 
     // Send to target color in global palette
-    setColorAtPosition(this.colorPosition.getEnum(), color);
-    setColorAtPosition(this.secondPosition.getEnum(), color2);
-    setColorAtPosition(this.thirdPosition.getEnum(), color3);
+    LXSwatch activeSwatch = this.lx.engine.palette.swatch;
+    setColorAtPosition(activeSwatch, this.colorPosition.getEnum(), color);
+    setColorAtPosition(activeSwatch, this.secondPosition.getEnum(), color2);
+    setColorAtPosition(activeSwatch, this.thirdPosition.getEnum(), color3);
+
+    LXSwatch cueSwatch = findOrCreateCueSwatch();
+    setColorAtPosition(cueSwatch, this.colorPosition.getEnum(), color);
+    setColorAtPosition(cueSwatch, this.secondPosition.getEnum(), color2);
+    setColorAtPosition(cueSwatch, this.thirdPosition.getEnum(), color3);
   }
 
-  protected void setColorAtPosition(DmxColorModulator.ColorPosition colorPosition, int color) {
-    LXSwatch swatch = this.lx.engine.palette.swatch;
+  protected void setColorAtPosition(LXSwatch swatch, DmxColorModulator.ColorPosition colorPosition, int color) {
     if (colorPosition != DmxColorModulator.ColorPosition.NONE) {
       swatch.getColor(colorPosition.index).primary.setColor(color);
       swatch.getColor(colorPosition.index).mode.setValue(LXDynamicColor.Mode.FIXED);
     }
+  }
+
+  protected LXSwatch findOrCreateCueSwatch() {
+    LXPalette palette = this.lx.engine.palette;
+    if (this.lx.engine.palette.swatches.size() == 0) {
+      palette.saveSwatch();
+    }
+    LXSwatch cueSwatch = palette.swatches.get(0);
+    cueSwatch.label.setValue("CUE");
+    return cueSwatch;
   }
 }
