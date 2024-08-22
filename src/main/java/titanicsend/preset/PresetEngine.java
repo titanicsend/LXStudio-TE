502b5ab1b455f67b08921f05693f24265becf4d8
Justin Belcher <jkbelcher@gmail.com>
2024-08-22 16:09:48 -0700

    Import presets from file system with context menu

--- src/main/java/titanicsend/preset/PresetEngine.java
+++ src/main/java/titanicsend/preset/PresetEngine.java
@@ -1,9 +1,16 @@
 package titanicsend.preset;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import heronarts.lx.LX;
 import heronarts.lx.LXComponent;
 import heronarts.lx.LXPresetComponent;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -36,5 +43,50 @@ public class PresetEngine extends LXComponent {
       return null;
     }
     return LXComponent.getComponentName(((LXComponent)component).getClass());
+  }
+
+  /**
+   * Import
+   */
+
+  /**
+   * Import all file system presets for the current component (pattern) class
+   */
+  public void importPresets(LXPresetComponent component) { //}, UserPresetCollection collection) {
+    File presetFolder = this.lx.getPresetFolder((LXComponent)component);
+    File[] files = presetFolder.listFiles((dir, name) -> name.endsWith(".lxd"));
+    if (files != null) {
+      for (File file : files) {
+        try (FileReader fr = new FileReader(file)) {
+          JsonObject obj = new Gson().fromJson(fr, JsonObject.class);
+          String name = nameWithoutExtension(file);
+          UserPresetCollection collection = this.currentLibrary.get(component);
+          collection.addPreset(component, obj).setLabel(name);
+        } catch (IOException iox) {
+          LX.error("Could not load preset file: " + iox.getLocalizedMessage());
+          this.lx.pushError(iox, "Could not load preset file: " + iox.getLocalizedMessage());
+        } catch (Exception x) {
+          LX.error(x, "Exception loading preset file: " + x.getLocalizedMessage());
+          this.lx.pushError(x, "Exception in importPresets: " + x.getLocalizedMessage());
+        }
+      }
+    }
+  }
+
+  /**
+   * Import all file system presets for all patterns
+   */
+  public void importAllPatternPresets() {
+    // TODO
+  }
+
+  private String nameWithoutExtension(File file) {
+    String name = file.getName();
+    int d = name.lastIndexOf('.');
+    if (d > 0 && d < name.length() - 1) {
+      return name.substring(0, d);
+    } else {
+      return name;
+    }
   }
 }
