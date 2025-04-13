ce544b3117865702ae6970ae836614a9bd8cec09
Andrew Look <alook@makenotion.com>
2025-04-12 22:25:02 -0400

    script, resources, target

--- cleanup.sh
+++ cleanup.sh
@@ -5,37 +5,31 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "$(readlink -f "$0")" )" &> /dev/null && pwd )"
 
 
-# For 'Logs' and 'Autosave' dirs, move the content to the new subfolders
-if [ -d "$SCRIPT_DIR/Logs" ]; then
-    echo "Checking Logs"
-    if [ -z "$(ls -A "$SCRIPT_DIR/Logs")" ]; then
-        echo "  Empty; clearning"
-        rmdir "$SCRIPT_DIR/Logs"
-    else
-        echo "  Not empty; moving contents to ./te-app/Logs"
-        mkdir -p $SCRIPT_DIR/te-app/Logs/
-        mv $SCRIPT_DIR/Logs/* $SCRIPT_DIR/te-app/Logs/
-        rmdir "$SCRIPT_DIR/Logs"
-    fi
-fi
+# For dirs with auto-generated contents, move the content to the new subfolders
+MOVE_DIRS=("Logs" "Autosave" "target")
+
+for name in "${PHANTOM_DIRS[@]}"; do
+    echo "Checking $name"
+    cur_dir="$SCRIPT_DIR/$name"
 
 
-if [ -d "$SCRIPT_DIR/Autosave" ]; then
-    echo "Checking Autosave"
-    if [ -z "$(ls -A "$SCRIPT_DIR/Autosave")" ]; then
-        echo "  Empty; clearning"
-        rmdir "$SCRIPT_DIR/Autosave"
-    else
-        echo "  Not empty; moving contents to ./te-app/Autosave"
-        mkdir -p $SCRIPT_DIR/te-app/Autosave/
-        mv $SCRIPT_DIR/Autosave/* $SCRIPT_DIR/te-app/Autosave/
-        rmdir "$SCRIPT_DIR/Autosave"
+    if [ -d "$cur_dir" ]; then
+        echo "Checking $name"
+        if [ -z "$(ls -A "$cur_dir")" ]; then
+            echo "  Empty; clearning"
+            rmdir "$cur_dir"
+        else
+            echo "  Not empty; moving contents to ./te-app/$name"
+            mkdir -p $SCRIPT_DIR/te-app/$name/
+            mv $cur_dir/* $SCRIPT_DIR/te-app/$name/
+            rmdir "$cur_dir"
+        fi
     fi
-fi
+done
 
 
 # Define the directories to check, where we don't want to auto-move the files
-PHANTOM_DIRS=("Fixtures" "Projects" "Models" "Colors" "Presets")
+PHANTOM_DIRS=("Fixtures" "Projects" "Models" "Colors" "Presets" "script" "resources")
 
 
 for name in "${PHANTOM_DIRS[@]}"; do
