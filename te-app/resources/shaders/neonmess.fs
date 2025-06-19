4d80522ec2fe56d7ef0c917fe5bcb42362af0459
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2025-06-18 19:06:11 -0700

    checkpoint

--- te-app/resources/shaders/neonmess.fs
+++ te-app/resources/shaders/neonmess.fs
@@ -1,56 +1,80 @@
 #pragma name "NeonMess"
 
-#pragma TEControl.SIZE.Range(2.0,0.5,10)
+#pragma TEControl.SIZE.Range(0.4,1.1,0.25)
+#pragma TEControl.WOW1.Range(0.25,0.0,1.0)
+//#pragma iChannel1 "resources/shaders/textures/fontgenerator.png"
 
 #include <include/constants.fs>
 #include <include/colorspace.fs>
+//#include <include/debug.fs>
 
-// Euler axis/angle rotation.  Very handy.
+#define ITERATIONS 100.0
+
+// 3D axis/angle rotation from FabriceNeyret2
+// (comment from https://www.shadertoy.com/view/WtjcDt)
+// takes the point to be rotated, a vector describing the axis of rotation,
+// and the angle to rotate by in radians.
 #define rot3D(p,axis,t) mix(axis*dot(p,axis),p,cos(t))+sin(t)*cross(p,axis)
-
-// cosine palette mapping
-#define H(h)  (cos( h/2. + vec3(31,10,20) ) * .6)
-
-// output scaling
-#define L(c)  log(1.+c*c)
 
 void mainImage( out vec4 O, vec2 U) {
     O = vec4(0);
-    vec3 c=vec3(0);
+    float acc = 0.0;
+    float beatWave = 0.0;
+    float signal = bassLevel;
 
-    vec3 rd = normalize( vec3(U-.5*iResolution.xy, iResolution.y))*10.;
-    rd *= iScale;
+    vec2 uv = U / iResolution.xy;
+
+    vec3 rd = normalize( vec3(U-.5*iResolution.xy, iResolution.y))*20.;
 
     float sc,dotp,totdist=0.,
-    t=iTime / 3.;
+    t = iTime,
+    // avoid starting at 0 angle. Way too bright.
+    theta = -iRotationAngle + 0.1;
 
-    for (float i = 0.; i < 150.; i++) {
+
+    for (float i = 0.; i < ITERATIONS; i++) {
         vec4 p = vec4(rd * totdist, 0.);
+        beatWave = 0.0;
 
-        p.xyz += vec3(0,0,-18.);
-        p.xyz = rot3D(p.xyz, normalize(vec3(sin(t / 7.),cos(t / 11.),0)), -iRotationAngle);
-        sc = 1.;  //scale factor
+        // center the point in the scene, a short distance from the camera
+        p.xyz += vec3(0,0,-18);
+        p.xyz = rot3D(p.xyz, normalize(vec3(sin(t / 7.),cos(t / 11.),0.)), theta);
+        sc = 1.0;
 
         for (float j = 0.; j < 6.; j++) {
-            p = abs(p) * 0.56 -  vec4(.025 * cos(p.xy / 5.), .01 * sin(p.zw / 3.));
+            // field generator - offset point by sin/cos curve
+            p = abs(p) * 0.56 -
+              vec4(.025 * cos(t / 3. + p.xy / 6.0), .015 * sin(t / 2 - p.zw / 3.0));
 
-            dotp = max(1. / dot(p, p),.4);
+            dotp = max(1. / dot(p, p),iScale);
             sc *= dotp ;
 
             p.zw = length(p.xz) < length(p.zw) ? p.xz : p.zw;  //reflection
+
+
+            if (abs(sc - signal) < 0.1) {
+                beatWave -= min(.006,signal / 100.0);
+            }
+
             p = abs(p) * dotp - 1.;
         }
 
         float dist = abs(length(p) - .1) / sc; // distance estimate
-        float stepsize = dist + 0.0001;        // distance plus a little extra
+        float stepsize = dist + 0.0001;        // plus a little extra
         totdist += stepsize;                   // move the distance along rd
 
-        //accumulate color, fading with distance and iteration count
-        c += mix(vec3(1), H(L(sc)),.6) * .014 *  exp(-i*i*stepsize*stepsize/2.);
+        //accumulate density, fading with distance and iteration count
+        acc += ((levelReact * beatWave) + stepsize) * exp(-i*i*stepsize*stepsize/ 2.0);
     }
 
-    c *= c;
-    c = 1. - exp(-c);
-    float b = 1.25 * max(c.r, max(c.g, c.b)); // brightness
-    O = vec4(getGradientColor(b),b * b);
+
+
+    acc = 1.0 - ( acc);
+    float bri = smoothstep(-0.5 + iWow1, 1.0, acc);
+    O = vec4(getGradientColor(fract(acc)),bri);
+
+    // Debug print: Show the signal level on the car
+    //float x = print(iChannel1, uv - vec2(0.15, 0.2), signal, 0.075, 2.0, 4.0);
+    //vec4 txt = x * vec4(0.0,1.0,1.0,1.0);
+    //O = mix(O, txt, txt.a);
 }
