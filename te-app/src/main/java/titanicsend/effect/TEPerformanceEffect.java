ededba0e3599b5c11516d5595679101891a81704
Andrew Look <alook@makenotion.com>
2025-07-29 01:13:15 -0400

    PerfEffect

---
+++ te-app/src/main/java/titanicsend/effect/TEPerformanceEffect.java
@@ -0,0 +1,38 @@
+package titanicsend.effect;
+
+import heronarts.lx.LX;
+import heronarts.lx.parameter.LXListenableNormalizedParameter;
+
+/** Effects for which we want easy trigger access on a dedicated MIDI controller. */
+public abstract class TEPerformanceEffect extends TEEffect {
+
+  public TEPerformanceEffect(LX lx) {
+    super(lx);
+  }
+
+  // Knobs to expose
+  protected abstract LXListenableNormalizedParameter primaryParam();
+
+  protected abstract LXListenableNormalizedParameter secondaryParam();
+
+  // Behavior when triggered
+  protected abstract void trigger();
+
+  // Idea: use these to annotate certain effects if they're only safe for GPU/CPU
+  /*
+  default boolean gpuMixerCompatible() {
+    return true;
+  }
+
+  default boolean cpuMixerCompatible() {
+    return true;
+  }
+  */
+
+  // Idea: for anything TE-model specific (e.g. BassLighting)
+  /*
+  default boolean teModelOnly() {
+    return false;
+  }
+  */
+}
