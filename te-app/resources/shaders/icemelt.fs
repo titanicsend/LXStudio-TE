6a49ee013359f4e6b0d3428f277438b8481c8a34
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2025-08-07 12:47:05 -0700

    Update outrun grid

--- te-app/resources/shaders/icemelt.fs
+++ te-app/resources/shaders/icemelt.fs
@@ -1,7 +1,17 @@
-// Probably the sillies shader I've made so far.
+// This is a silly shader, but it looks really nice.
 // Water running down a melting ice cliff.
 #pragma name "Icemelt"
 #pragma iChannel1 "resources/shaders/textures/icecliff.png"
+
+#pragma TEControl.QUANTITY.Range(0.6,0.2,1.0)
+#pragma TEControl.LEVELREACTIVITY.Disable
+#pragma TEControl.FREQREACTIVITY.Disable
+#pragma TEControl.ANGLE.Disable
+#pragma TEControl.SPIN.Disable
+#pragma TEControl.SIZE.Disable
+#pragma TEControl.WOW1.Disable
+#pragma TEControl.WOW2.Disable
+#pragma TEControl.WOWTRIGGER.Disable
 
 float random(float x) {
     return fract(sin(x) * 10000.);
@@ -43,23 +53,34 @@ float waterNoise(vec2 p) {
     float y = movingNoise(p + 100.);
     return movingNoise(p + vec2(x, y));
 }
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     float t = 20. + iTime;
     vec2 uv = fragCoord.xy / iResolution.xy;
-    float n = waterNoise(vec2(0.0, iTime) + uv * (vec2(60.0,60.0)));
+    float n = waterNoise(vec2(0.0, iTime) + uv * (vec2(40.0,20.0)));
 
     // Optional: make a Ken Burns documentary (w/slow pan) for this texture!
-    //uv.x += 0.1 * sin(iTime / 16.0);
+    uv.x = fract(uv.x + 0.1 * sin(iTime / 32.0));
 
-    // no water on very prominent ice features
+    // Sample the texture, which is largely blue and white. We'll use
+    // the red channel as a proxy for the a bump map of the ice cliff.
+    // The less red is present, the farther from white we'll be.  This
+    // makes a good appoximation of the areas where water would naturally
+    // flow.
     vec3 under = texture(iChannel1,uv).rgb;
-    float val = under.r * under.r * under.r;
-    n = (val >= iWow1) ? n * max(0.0,iWow1 - val) : n;
-    n = mix(0.0,n,iWow1 / (val + 0.0006));
+    float val = under.r * under.r;
+    // Layer water noise over the texture in low areas
+    float mask = smoothstep(iQuantity - 0.05, iQuantity + 0.05, val);
+    n *= 1.0 - mask;
 
+    // Now sample the texture again using the water noise value to
+    // calculate offset due to "refraction" from the water.
     under = texture(iChannel1,uv + (n / 52.)).rgb;
-    under = under * under * under; // increase contrast
 
+    // Ice is pretty white -- let's make detail more visible by
+    // increase the contrast of the final image.
+    under = under * under * under;
 
-    fragColor = vec4(mix(under, vec3(.1, .2, 1.), n / 24.0),1.0);
+    // mix texture color with water color value for final color.
+    fragColor = vec4(mix(under, vec3(.1, .3, 1.), n / 24.0),1.0);
 }
