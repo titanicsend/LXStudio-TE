980a861115a93f8281b26e35282e920fe14ecc84
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2025-08-10 10:45:14 -0700

    Update FollowThatStar

--- te-app/resources/shaders/followthatstar.fs
+++ te-app/resources/shaders/followthatstar.fs
@@ -1,7 +1,7 @@
 #include <include/constants.fs>
 #include <include/colorspace.fs>
 
-float step = 0.05;   // star spacing
+float spacing = 0.2;   // star spacing
 float indent = 0.06;     // shape indent for stars
 float numPoints = 5.;
 
@@ -11,14 +11,20 @@ mat2 r2d(float a) {
     return mat2(c, s, -s, c);
 }
 
-// Minkowski distance at fractional exponents makes the nice 4-pointed star!
-float minkowskiDistance(vec2 uv, float p) {
-    return pow(pow(abs(uv.x), p) + pow(abs(uv.y), p), 1.0 / p);
-}
-
-// Probably not really that random...
+// Random-ish number in the range [-1, 1]
 float rand(vec2 p) {
     return fract(sin(dot(p, vec2(12.543,514.123)))*4732.12);
+}
+
+// sawtooth to [0,1] sinusoidal wave
+float wave(float n) {
+    return 0.5+(sin(TAU * abs(fract(n))) * 0.5);
+}
+
+// Minkowski distance at fractional exponents makes the nice 4-pointed star!
+// NOTE: Not currently used, but keeping here for reference.
+float minkowskiDistance(vec2 uv, float p) {
+    return pow(pow(abs(uv.x), p) + pow(abs(uv.y), p), 1.0 / p);
 }
 
 // New star SDF from https://www.shadertoy.com/view/4tfGWr
@@ -40,55 +46,54 @@ float drawStar(vec2 o, float size, float startAngle) {
     vec2 outline;
     outline.y = sin(angle);
     outline.x = cos(angle);
-
     float dist = abs(dot(outline, q));
 
-    float ss = size; // * (1. + 0.2 * sin(iTime * hash(size) * 20. ) );
+    float ss = size;
     float r = numPoints * ss;
 
-    float star = smoothstep( r, r + 0.005, dist );
-    return star;
+    return smoothstep( r, r + 0.005, dist );
 }
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     vec3 color = vec3(0.0);
 
     // normalize and shift origin to center (-0.5-0.5 coordinate range)
     vec2 uPos = -0.5+(fragCoord.xy / iResolution.xy );
 
-    // star size pulses with the music!
-    float pulse = iScale * ((iWow1 * (-0.5+beat)) + .618);
-    float dance = 0.0025 + (0.5 * bassLevel * iWow1);
-    float brightness = 0.;
-
     vec3 starColor;
     vec3 finalColor = vec3(0.0);
-    float tOffset = 0.0;
-    float starTime = iTime / 8.0;
+    float t = iTime / 8.0;
     float v = iQuantity * 2.0;
 
     for (float j = 0; j < v; j++) {
-        tOffset += TAU * 10.618;
-        starColor = getGradientColor(j / v);
+        // each group of stars gets its own starting time offset
+        t += TAU * .618;
+        // and its own color (higher iWow2 == more monochromeness)
+        starColor = mix(getGradientColor(j / v),iColorRGB,iWow2);
         for (float i = 1.; i <= v; i++) {
-            // time offset for each star.
-            float d = 0.05 * i;
-            float t = (tOffset + starTime) + d;
-            float pulse = iWow1 * beat;
+            float pct = 0.5 * step (0.5, i/v);
+            // shape the beat sawtooth so its peak will be very short
+            float pulse = 1.0 - fract(beat + pct);
+            pulse = iWow1 * pulse * pulse;
+            // distance offset for each star.
+            float d = spacing * i;
 
             // calculate point offset and rescale for our normalized coord range
             vec2 point = cos(t*d)*vec2(cos(t*1.5), sin(t*3.) ) + sin(t*d) * vec2( cos(t*2.), sin(t*.75) );
             point = uPos - point / 2.2;
 
-            // if we're rotating, give the individual stars slightly different rates
-            float theta = iRotationAngle + 0.5 * i * j;
+            // hash value based on [i,j], precalculated since we use it twice.  Saves
+            // a call to rand() in the inner loop.
+            float rij = rand(vec2(i,j));
 
-            // add brightness contribution of each star
-            float beatBlink = (rand(vec2(i,j)) < 0.5) ? 1.0 - pulse : 1.0;
-            float beatScale = (rand(vec2(j,i)) < 0.5) ? iScale + 2.0 * pulse : iScale;
+            // give the individual stars slightly different spin rates
+            float theta = iRotationAngle * rij;
+
+            // add brightness contribution of each star, including beat reactivity
+            float beatScale = (abs(rand(vec2(j,i))) < 0.6) ? iScale + 2.0 *  pulse : iScale;
+            float beatBlink = (abs(rij) < 0.2) ? 1.0 + iWow1 * (1.0 - wave(4.0 * pulse)) : 1.0;
             float starBright = beatBlink * (1.0 - drawStar(point, 0.001 * beatScale, theta));
             finalColor = mix(finalColor, starColor * starBright, starBright);
-
-
         }
     }
 
--- te-app/src/main/java/titanicsend/pattern/jon/FollowThatStar.java
+++ te-app/src/main/java/titanicsend/pattern/jon/FollowThatStar.java
@@ -16,8 +16,8 @@ public class FollowThatStar extends GLShaderPattern {
         .setRange(TEControlTag.QUANTITY, 5, 1, 30)
         .setUnits(TEControlTag.QUANTITY, LXParameter.Units.INTEGER);
 
-    controls.setRange(TEControlTag.SIZE, 1.75, 1.0, 5);
-    controls.setRange(TEControlTag.WOW2, 1, 1, 4);
+    controls.setRange(TEControlTag.SIZE, 2., .5, 5);
+    controls.setRange(TEControlTag.SPIN, .333, -3.0, 3.0);
     controls.markUnused(controls.getLXControl(TEControlTag.LEVELREACTIVITY));
     controls.markUnused(controls.getLXControl(TEControlTag.FREQREACTIVITY));
     controls.markUnused(controls.getLXControl(TEControlTag.WOWTRIGGER));
