a38bd4f08dea337fc7830064239b362248e74c45
Justin Belcher <jkbelcher@gmail.com>
2025-09-06 18:42:55 -0600

    APC40: Shift+Knob to browse UserPresets (#758)
    * APC40: Shift+Knob to browse UserPresets
    
    * Store knob value while shift is pressed to avoid jumpy pattern browsing

--- te-app/src/main/java/titanicsend/lx/APC40Mk2.java
+++ te-app/src/main/java/titanicsend/lx/APC40Mk2.java
@@ -7,15 +7,18 @@ import heronarts.lx.midi.MidiControlChange;
 import heronarts.lx.midi.MidiNote;
 import heronarts.lx.midi.MidiNoteOn;
 import heronarts.lx.midi.surface.LXMidiSurface;
-import heronarts.lx.mixer.LXBus;
+import heronarts.lx.mixer.LXAbstractChannel;
 import heronarts.lx.mixer.LXChannel;
 import heronarts.lx.parameter.BooleanParameter;
 import heronarts.lx.parameter.LXParameterListener;
+import heronarts.lx.pattern.LXPattern;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import titanicsend.pattern.TEPerformancePattern;
 
 @LXMidiSurface.Name("Akai APC40 mkII TE")
 @LXMidiSurface.DeviceName("APC40 mkII")
@@ -58,9 +61,16 @@ public class APC40Mk2 extends heronarts.lx.midi.surface.APC40Mk2 {
 
   private static final List<APC40Mk2> surfaces = new ArrayList<APC40Mk2>();
 
+  private boolean shiftOn = false;
+
+  private int[] channelKnobValues = new int[CHANNEL_KNOB_NUM];
+
   public APC40Mk2(LX lx, LXMidiInput input, LXMidiOutput output) {
     super(lx, input, output);
     surfaces.add(this);
+
+    // Initialize channel knob values
+    Arrays.fill(this.channelKnobValues, 0);
   }
 
   @Override
@@ -162,6 +172,20 @@ public class APC40Mk2 extends heronarts.lx.midi.surface.APC40Mk2 {
   private boolean noteReceived(MidiNote note, boolean on) {
     int pitch = note.getPitch();
 
+    // Track shift state (temporary until available from upstream)
+    if (pitch == SHIFT) {
+      this.shiftOn = on;
+      if (on) {
+        // TODO: Send normalized value for selected preset? So far it seems okay...
+      } else {
+        // When shift is release, send (restore) channel knob values for pattern browsing
+        for (int i = 0; i < CHANNEL_KNOB_NUM; i++) {
+          sendControlChange(0, CHANNEL_KNOB + i, this.channelKnobValues[i]);
+        }
+      }
+      return false;
+    }
+
     if (on) {
       for (UserButton userButton : UserButton.values()) {
         if (pitch == userButton.note) {
@@ -187,22 +211,42 @@ public class APC40Mk2 extends heronarts.lx.midi.surface.APC40Mk2 {
   }
 
   @Override
+  public void noteOffReceived(MidiNote note) {
+    if (noteReceived(note, false)) {
+      return;
+    }
+    super.noteOffReceived(note);
+  }
+
+  @Override
   public void controlChangeReceived(MidiControlChange cc) {
     int number = cc.getCC();
 
     // TE: Channel knobs set focused pattern, if fader is down.
     if (number >= CHANNEL_KNOB && number <= CHANNEL_KNOB_MAX) {
-      int channel = number - CHANNEL_KNOB;
-      if (channel < this.lx.engine.mixer.channels.size()) {
-        LXBus bus = this.lx.engine.mixer.channels.get(channel);
-        if (bus instanceof LXChannel && bus.fader.getValue() == 0) {
-          int numPatterns = ((LXChannel) bus).patterns.size();
-          if (numPatterns > 0) {
-            double normalized = cc.getNormalized();
-            // Set active pattern
-            ((LXChannel) bus).goPatternIndex((int) (normalized * (numPatterns - 1)));
-            // Alternative for focused pattern
-            // ((LXChannel)bus).focusedPattern.setNormalized(normalized);
+      int channelIndex = number - CHANNEL_KNOB;
+      if (channelIndex < this.lx.engine.mixer.channels.size()) {
+        LXAbstractChannel abstractChannel = this.lx.engine.mixer.channels.get(channelIndex);
+        if (abstractChannel instanceof LXChannel channel && isNotLive(abstractChannel)) {
+          // Knob corresponds to channel, and channel is not live.
+          if (this.shiftOn) {
+            // Change user preset on selected pattern
+            LXPattern pattern = channel.getActivePattern();
+            if (pattern instanceof TEPerformancePattern tePattern) {
+              tePattern.getControls().getPresetSelectorOffair().setNormalized(cc.getNormalized());
+            }
+          } else {
+            // Remember knob position in case shift is pressed
+            this.channelKnobValues[channelIndex] = cc.getValue();
+            // Change active pattern on the channel
+            int numPatterns = channel.patterns.size();
+            if (numPatterns > 0) {
+              double normalized = cc.getNormalized();
+              // Set active pattern
+              channel.goPatternIndex((int) (normalized * (numPatterns - 1)));
+              // Alternative for focused pattern
+              // ((LXChannel)bus).focusedPattern.setNormalized(normalized);
+            }
           }
         }
       }
@@ -210,6 +254,10 @@ public class APC40Mk2 extends heronarts.lx.midi.surface.APC40Mk2 {
     }
 
     super.controlChangeReceived(cc);
+  }
+
+  private boolean isNotLive(LXAbstractChannel abstractChannel) {
+    return abstractChannel.fader.getValue() == 0 || !abstractChannel.enabled.isOn();
   }
 
   @Override
